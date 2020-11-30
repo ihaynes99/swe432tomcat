c41832fcd6083695e2998bb63eae2adb4dc2bcb5
ihaynes99 <ihaynes@gmu.edu>
2020-11-30 16:45:21 -0500

    Update Java files

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -3,12 +3,21 @@ package servlet;
 import java.io.PrintWriter;
 import java.io.IOException;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletContext;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 
 @WebServlet(
         name = "MyServlet",
@@ -17,67 +26,81 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet
 {
+    static String Servlet = "HelloServletForward";
+   // static String firstVar;
+   // static String operator;
+   // static String secondVar;
+   //static String RfFile = "persist.txt";
+    //static final String ValSep = ";";
+
   @Override
    protected void doGet  (HttpServletRequest req, HttpServletResponse res)
           throws ServletException, IOException
    {
-
+        HttpSession session = req.getSession(false);
         res.setContentType ("text/html");
         PrintWriter out = res.getWriter ();
 
         out.println ("<HTML>");
         out.println ("<HEAD>");
-        out.println ("<TITLE>Scott's simple servlet program</TITLE>");
+        out.println ("<TITLE>SWE 432 HW 8</TITLE>");
         out.println ("</HEAD>");
 
         out.println ("<BODY>");
-        out.println ("<CENTER>");
+        out.println ("<HR>");
+        out.println ("<H1> SWE 432 HW 8 By: Pooja Ramesh & Scott Haynes</H1>");
+        out.println ("<HR>");
+        out.println("<form method = \"POST\" action = \"/" + Servlet + "\">"); //calls do-post method
+        out.println(" <LABEL for = FirstVarNm>First Variable Name:");
+        out.println(" <INPUT type = text id = FirstVarNm name = FirstVarNm>");
 
-        out.println (" <B>Hello! This is Scott Haynes. How are you?</B><BR> <!--  English -->");
-        out.println (" <B>Alo!</B><BR> <!--  Portuguese -->");
-        out.println (" <B>Anyong haseyo!</B><BR> <!--  Korean -->");
-        out.println (" <B>Apa Kabar!</B><BR> <!--  Indonesian -->");
-        out.println (" <B>Ave!</B><BR> <!--  Latin -->");
-        out.println (" <B>Bon jour!</B><BR> <!--  French -->");
-        out.println (" <B>Ciao!</B><BR> <!--  Italian -->");
-        out.println (" <B>Hajur!</B><BR> <!--  Nepali (India) -->");
-        out.println (" <B>Hallo!</B><BR> <!--  German -->");
-        out.println (" <B>Hej!</B><BR> <!--  Swedish -->");
-        out.println (" <B>Hei!</B><BR> <!--  Norwegian -->");
-        out.println (" <B>Hola!</B><BR> <!--  Spanish -->");
-        out.println (" <B>Kaise ho!</B><BR> <!--  Hindi (depending on who you believe) -->");
-        out.println (" <B>Kem Chho, Kem Che!</B><BR> <!--  Gujurati (India) -->");
-        out.println (" <B>Ki Khobor!</B><BR> <!--  Bengali -->");
-        out.println (" <B>Marhaba!</B><BR> <!--  Arabic -->");
-        out.println (" <B>Moin Moin!</B><BR> <!--  German, Hamburg -->");
-        out.println (" <B>Moni!</B><BR> <!--  Chichewa -->");
-        out.println (" <B>Namaskaram!</B><BR> <!--  Telugu (India) -->");
-        out.println (" <B>Namskar!</B><BR> <!--  Hindi -->");
-        // Why do I keep getting different words in Hindi?
-        // out.println (" <B>Namaste!</B><BR> <!--  Hindi -->");
-        // out.println (" <B>Namastar!</B><BR> <!--  Hindi -->");
-        out.println (" <B>Konnichiwa!</B><BR> <!--  Japanese -->");
-        out.println (" <B>Manh gioi!</B><BR> <!--  Vietnamese -->");
-        out.println (" <B>Marhabai!</B><BR> <!--  Arab -->");
-        out.println (" <B>Ni hao!</B><BR> <!--  Chinese -->");
-        out.println (" <B>Shalom!</B><BR> <!--  Hebrew -->");
-        out.println (" <B>Sallam!</B><BR> <!--  Persian -->");
-        out.println (" <B>Sat-Sri-Akal!</B><BR> <!--  Punjabi (India) -->");
-        out.println (" <B>Strasvedte!</B><BR> <!--  Russian -->");
-        out.println (" <B>Yah-shimu-siz!</B><BR> <!--  Uighur -->");
-        out.println (" <B>Zhao  Shen!</B><BR> <!-- Cantonese -->");
+        out.println(" <LABEL for = operators>Choose a operator:");
+        out.println(" <SELECT name = operators id= operators>");
+        out.println(" <OPTION value = and>&amp;&amp;");
+        out.println(" <OPTION value = or>||");
+        out.println(" <OPTION value = xor>^");
+        out.println("</SELECT>");
+        out.println("<LABEL for = SecondVarNm>Second Variable Name:");
+        out.println(" <INPUT type = text id = SecondVarNm name = SecondVarNm>");
+        out.println("<BR>");
+        out.println("<BR>");
 
-        out.println ("<P>");
-        out.println ("For a longer listing of \"hellos\" to the world, go see");
-        out.println ("<A HREF=\"http://www.ipl.org/youth/hello/\">http://www.ipl.org/youth/hello/</A>");
+        out.println("<INPUT type = submit name = bttn1 value = Submit>");
+        out.println("</form>");
+    /*    out.println("<br>");
+        out.println("<br>");
+        out.println("<LABEL for = CollabSum5>Collabaration Summary Assign5: Pooja worked on the doGet method (accepting user input and passing it) while Scott worked on the doPost ensuring that the truth table was printed out. Pooja also typed out this collaboration summary");
+        out.println("<br>");
+        out.println("<br>");
+        out.println("<LABEL for = CollabSum7>Collabaration Summary Assign7: Pooja worked on ensuring that the list of previous attempts during the session are displayed correctly while Scott ensured that the data was saved. Pooja also typed this collaboration summary");
+*/
 
-        out.println ("</CENTER>");
+
         out.println ("</BODY>");
 
         out.println ("</HTML>");
-        out.flush();
 
+
+
+        out.flush();
         out.close ();
 
     }
+     @Override
+     public void doPost (HttpServletRequest request, HttpServletResponse response)
+             throws ServletException, IOException
+     {
+         request.setAttribute("FirstVarNm", "test");
+
+
+         request.getSession().setAttribute("operators", "test");
+         request.getSession().setAttribute("SecondVarNm", request.getParameter("SecondVarNm"));
+
+          if (request.getParameter("FirstVarNm") != null && request.getParameter("operators") != null && request.getParameter("SecondVarNm") != null)
+          {
+             RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/servlet2");
+             dispatcher.forward(request,response);
+          }
+
+     }
 }
---
+++ src/main/java/servlet/HelloServletForward.java
@@ -0,0 +1,249 @@
+package servlet;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(
+        name = "HelloServletForward",
+        urlPatterns = {"/HelloServletForward"}
+    )
+
+public class HelloServletForward extends HttpServlet
+{
+     public static String firstVar = "";
+     public static String operation = "";
+     public static String secondVar = "";
+     //static final String ValSep = ";";
+
+  @Override
+   protected void doGet  (HttpServletRequest request, HttpServletResponse response)
+          throws ServletException, IOException
+   {
+     //  String firstVar = "";
+      // String operation = "";
+      // String secondVar = "";
+
+       firstVar = (String) request.getSession().getAttribute("FirstVarNm");
+       operation = (String) request.getSession().getAttribute("operators");
+       secondVar = (String) request.getSession().getAttribute("SecondVarNm");
+
+       boolean or = false;
+       boolean and = false;
+       boolean not = false;
+       boolean xor = false;
+
+
+
+
+       switch (operation)
+       {
+           case "~":
+           case "not":
+               operation = "~";
+               not = true;
+               break;
+           case "||":
+           case "or":
+               operation = "||";
+               or = true;
+               break;
+           case "&&":
+           case "and":
+               operation = "&&";
+               and = true;
+               break;
+           case "xor":
+               operation = "xor";
+               xor = true;
+               break;
+
+       }
+
+       PrintWriter out = response.getWriter();
+       out.println ("<HTML>");
+       out.println ("<BODY>");
+      // out.println("<p>hehehe test</p>");
+       out.println("<LABEL for = echo>Echo: "+firstVar+" "+operation+" "+secondVar+"");
+
+       out.println ("</BODY>");
+       out.println ("</HTML>");
+       out.flush();
+       out.close();
+
+       //request.getSession().setAttribute("FirstVarNm", session.getAttribute("FirstVarNm"));
+
+       //session.setAttribute("operators", operation);
+       //session.setAttribute("SecondVarNm", session.getAttribute("SecondVarNm"));
+
+
+
+       //doPost(req,res);
+   }
+     @Override
+     public void doPost (HttpServletRequest request, HttpServletResponse response)
+             throws ServletException, IOException
+     {
+
+        // doGet(request,response);
+         String efirstVar = (String) request.getAttribute("FirstVarNm");
+         String eoperation = (String) request.getAttribute("operators");
+         String esecondVar = (String) request.getAttribute("SecondVarNm");
+
+          boolean or = false;
+          boolean and = false;
+          boolean not = false;
+          boolean xor = false;
+
+
+
+         /* switch (operation)
+          {
+              case "~":
+              case "not":
+                  operation = "~";
+                  not = true;
+                  break;
+              case "||":
+              case "or":
+                  operation = "||";
+                  or = true;
+                  break;
+              case "&&":
+              case "and":
+                  operation = "&&";
+                  and = true;
+                  break;
+              case "xor":
+                  operation = "xor";
+                  xor = true;
+                  break;
+
+          }*/
+
+         PrintWriter out = response.getWriter ();
+         out.println ("<HTML>");
+         out.println ("<BODY>");
+         out.println("<p>hehehe test</p>");
+         out.println("<LABEL for = echo>Echo: "+efirstVar+" "+eoperation+" "+esecondVar+"");
+
+         out.println("<table>");
+         out.println("<tr>");
+         out.println("<td>row:");
+         out.println("<td> "+firstVar+"");
+         out.println("<td> "+secondVar+"");
+         out.println("<td> result");
+         out.println("</tr>");
+         if (and)
+         {
+             out.println("<tr>");
+             out.println("<td> 1.");
+             out.println("<td> true");
+             out.println("<td> true");
+             out.println("<td> true");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 2.");
+             out.println("<td> true");
+             out.println("<td> false");
+             out.println("<td> false");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 3.");
+             out.println("<td> false");
+             out.println("<td> true");
+             out.println("<td> false");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 4.");
+             out.println("<td> false");
+             out.println("<td> false");
+             out.println("<td> false");
+             out.println("</tr>");
+
+         }
+         else if(or)
+         {
+             out.println("<tr>");
+             out.println("<td> 1.");
+             out.println("<td> true");
+             out.println("<td> true");
+             out.println("<td> true");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 2.");
+             out.println("<td> true");
+             out.println("<td> false");
+             out.println("<td> true");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 3.");
+             out.println("<td> false");
+             out.println("<td> true");
+             out.println("<td> true");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 4.");
+             out.println("<td> false");
+             out.println("<td> false");
+             out.println("<td> false");
+             out.println("</tr>");
+         }
+         else if (xor)
+         {
+             out.println("<tr>");
+             out.println("<td> 1.");
+             out.println("<td> true");
+             out.println("<td> true");
+             out.println("<td> false");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 2.");
+             out.println("<td> true");
+             out.println("<td> false");
+             out.println("<td> true");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 3.");
+             out.println("<td> false");
+             out.println("<td> true");
+             out.println("<td> true");
+             out.println("</tr>");
+
+             out.println("<tr>");
+             out.println("<td> 4.");
+             out.println("<td> false");
+             out.println("<td> false");
+             out.println("<td> false");
+             out.println("</tr>");
+         }
+         out.println("</table>");
+         out.println ("</BODY>");
+         out.println ("</HTML>");
+         out.flush();
+         out.close();
+
+     }
+}
